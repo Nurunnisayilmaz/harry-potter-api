8f0b5e89987c154a776d7c2431eaba5b7ef39afe
nisa <nurunnisa9@gmail.com>
2022-05-17 14:21:29 +0300

    edit characters, movies and spells css and create route in app.js

--- harry-potter-client/src/App.js
+++ harry-potter-client/src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+    Outlet
+} from "react-router-dom";
 import Home from "./pages/home/home"
+import Character from "./pages/characters/characters";
+import Movie from "./pages/movies/movies";
+import Spell from "./pages/spells/spells";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
     return (
         <div className="App">
+            <div className="header">
+                <Header/>
+            </div>
 
-            <Home/>
+            <BrowserRouter>
+                <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/movies" element={<Movie />} />
+                        <Route path="/spells" element={<Spell />} />
+                        <Route path="/characters" element={<Character />} />
+                </Routes>
+            </BrowserRouter>
+
+            <div className="footer">
+                <Footer/>
+            </div>
 
         </div>
     );
